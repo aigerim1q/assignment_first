72006a7bdb0a68e50ff05aa912ef892a3bbd6e55
Toma <230434@astanait.edu.kz>
2024-04-19 11:44:03 +0500

    defense of first assignment

---
+++ src/defense.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class defense{
+    public static int count(int n ,int[] arr, int number) {
+        int counnt = 0;
+        if (n==1){
+            return 1;
+        }
+        if (arr[n]==number) {
+            return counnt+ count(n-1,arr,number);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+        for (int i=0; i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int number=sc.nextInt();
+
+        int result=count(n,arr,number);
+        System.out.println(result);
+
+        }
+
+
+
+}
+
