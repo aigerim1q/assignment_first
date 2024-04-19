effbe56b7ef5fe771c0a65ace5d3cd4cb1a5b469
Toma <230434@astanait.edu.kz>
2024-04-19 11:51:30 +0500

    neww defense of first assignment

---
+++ src/def.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class def{
+    public static int count(int n ,int[] arr, int number) {
+        int counnt = 0;
+        if (n==1){
+            return 1;
+        }
+        if (arr[n-1]==number) {
+            counnt=1;
+        }
+
+        return counnt+ count(n-1,arr,number);
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr= new int[n];
+        for (int i=0; i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int number=sc.nextInt();
+
+        int result=count(n,arr,number);
+        System.out.println(result);
+
+    }
+
+
+
+}
